0c2200ed6843408544386e2a3033eca25ee5ec51
Krzysztof Kuch <65242925+cycopro@users.noreply.github.com>
2022-01-06 22:54:23 +0100

    udate

--- mysketch.ino
+++ mysketch.ino
@@ -3,18 +3,21 @@ const int ledPin1 = 6;
 const int ledPin2 = 4;
 const int ledPin3 = 2;
 
-int i = 0;
+
 void setup(){
   pinMode(sensorPin, INPUT);
   pinMode(ledPin1, OUTPUT);
   pinMode(ledPin2, OUTPUT);
   pinMode(ledPin3, OUTPUT);
+  digitalWrite(ledPin1,LOW);
+  digitalWrite(ledPin2,LOW);
+  digitalWrite(ledPin3,LOW);
   
 }
 
 void loop(){
-  i = digitalRead(sensorPin);
-  if (i == HIGH) {
+  
+  if (digitalRead(sensorPin) == HIGH) {
     
     digitalWrite(ledPin1, !
     digitalRead(ledPin1));
